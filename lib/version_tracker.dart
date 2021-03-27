c00a7d763fdb0174b1c11b899e411d44bf75287a
Kevin Morelli <kev9318@gmail.com>
2021-03-27 20:07:13 -0300

    Change description, add example base and better comments

--- lib/version_tracker.dart
+++ lib/version_tracker.dart
@@ -3,67 +3,72 @@ library version_tracker;
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+/// Provides an easy way to track versions and builds
 class VersionTracker {
   final String _versionsKey = "VersionTracker.Versions";
   final String _buildsKey = "VersionTracker.Builds";
 
   /// Gets a value indicating whether this is the first time this app has ever been launched on this device.
-  bool _isFirstLaunchEver;
   bool get isFirstLaunchEver => _isFirstLaunchEver;
+  bool _isFirstLaunchEver;
 
   /// Gets a value indicating if this is the first launch of the app for the current version number.
-  bool _isFirstLaunchForCurrentVersion;
   bool get isFirstLaunchForCurrentVersion => _isFirstLaunchForCurrentVersion;
+  bool _isFirstLaunchForCurrentVersion;
 
   /// Gets a value indicating if this is the first launch of the app for the current build number.
-  bool _isFirstLaunchForCurrentBuild;
   bool get isFirstLaunchForCurrentBuild => _isFirstLaunchForCurrentBuild;
+  bool _isFirstLaunchForCurrentBuild;
 
   /// Gets the current version number of the app.
-  String _currentVersion;
   String get currentVersion => _currentVersion;
+  String _currentVersion;
 
   /// Gets the current build of the app.
-  String _currentBuild;
   String get currentBuild => _currentBuild;
+  String _currentBuild;
 
   /// Gets the version number for the previously run version.
-  String _previousVersion;
   String get previousVersion => _previousVersion;
+  String _previousVersion;
 
   /// Gets the build number for the previously run version.
-  String _previousBuild;
   String get previousBuild => _previousBuild;
+  String _previousBuild;
 
   /// Gets the version number of the first version of the app that was installed on this device.
-  String _firstInstalledVersion;
   String get firstInstalledVersion => _firstInstalledVersion;
+  String _firstInstalledVersion;
 
   /// Gets the build number of first version of the app that was installed on this device.
-  String _firstInstalledBuild;
   String get firstInstalledBuild => _firstInstalledBuild;
+  String _firstInstalledBuild;
 
   /// Gets the collection of version numbers of the app that ran on this device.
-  List<String> _versionHistory;
   List<String> get versionHistory => _versionHistory;
+  List<String> _versionHistory;
 
   /// Gets the collection of build numbers of the app that ran on this device.
-  List<String> _buildHistory;
   List<String> get buildHistory => _buildHistory;
+  List<String> _buildHistory;
 
   /// Determines if this is the first launch of the app for a specified version number.
-  bool isFirstLaunchForVersion(String version) => _currentVersion == version && _isFirstLaunchForCurrentVersion;
+  bool isFirstLaunchForVersion(String version) =>
+      _currentVersion == version && _isFirstLaunchForCurrentVersion;
 
   /// Determines if this is the first launch of the app for a specified build number.
-  bool isFirstLaunchForBuild(String build) => _currentBuild == build && _isFirstLaunchForCurrentBuild;
+  bool isFirstLaunchForBuild(String build) =>
+      _currentBuild == build && _isFirstLaunchForCurrentBuild;
 
+  /// Start tracking versions and builds
   void track() async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     PackageInfo packageInfo = await PackageInfo.fromPlatform();
 
     Map<String, List<String>> versionTrail = Map<String, List<String>>();
 
-    _isFirstLaunchEver = !sharedPreferences.containsKey(_versionsKey) || !sharedPreferences.containsKey(_buildsKey);
+    _isFirstLaunchEver = !sharedPreferences.containsKey(_versionsKey) ||
+        !sharedPreferences.containsKey(_buildsKey);
     if (_isFirstLaunchEver)
       versionTrail.addAll({_versionsKey: [], _buildsKey: []});
     else
@@ -75,14 +80,19 @@ class VersionTracker {
     _currentVersion = packageInfo.version;
     _currentBuild = packageInfo.buildNumber;
 
-    _isFirstLaunchForCurrentVersion = !versionTrail[_versionsKey].contains(_currentVersion);
-    if (_isFirstLaunchForCurrentVersion) versionTrail[_versionsKey].add(_currentVersion);
+    _isFirstLaunchForCurrentVersion =
+        !versionTrail[_versionsKey].contains(_currentVersion);
+    if (_isFirstLaunchForCurrentVersion)
+      versionTrail[_versionsKey].add(_currentVersion);
 
-    _isFirstLaunchForCurrentBuild = !versionTrail[_buildsKey].contains(_currentBuild);
-    if (_isFirstLaunchForCurrentBuild) versionTrail[_buildsKey].add(_currentBuild);
+    _isFirstLaunchForCurrentBuild =
+        !versionTrail[_buildsKey].contains(_currentBuild);
+    if (_isFirstLaunchForCurrentBuild)
+      versionTrail[_buildsKey].add(_currentBuild);
 
     if (_isFirstLaunchForCurrentVersion || _isFirstLaunchForCurrentBuild) {
-      _writeHistory(sharedPreferences, _versionsKey, versionTrail[_versionsKey]);
+      _writeHistory(
+          sharedPreferences, _versionsKey, versionTrail[_versionsKey]);
       _writeHistory(sharedPreferences, _buildsKey, versionTrail[_buildsKey]);
     }
 
@@ -94,14 +104,17 @@ class VersionTracker {
     _buildHistory = versionTrail[_buildsKey].toList();
   }
 
+  // Show all the available data in a formatted string
   @override
   String toString() {
     var sb = StringBuffer();
     sb.writeln();
     sb.writeln('VersionTracker');
     sb.writeln('IsFirstLaunchEver:              $_isFirstLaunchEver');
-    sb.writeln('IsFirstLaunchForCurrentVersion: $_isFirstLaunchForCurrentVersion');
-    sb.writeln('IsFirstLaunchForCurrentBuild:   $_isFirstLaunchForCurrentBuild');
+    sb.writeln(
+        'IsFirstLaunchForCurrentVersion: $_isFirstLaunchForCurrentVersion');
+    sb.writeln(
+        'IsFirstLaunchForCurrentBuild:   $_isFirstLaunchForCurrentBuild');
     sb.writeln();
     sb.writeln('CurrentVersion:                 $_currentVersion');
     sb.writeln('PreviousVersion:                $_previousVersion');
@@ -115,9 +128,11 @@ class VersionTracker {
     return sb.toString();
   }
 
-  List<String> _readHistory(SharedPreferences preferences, String key) => preferences.getString(key).split('|');
+  List<String> _readHistory(SharedPreferences preferences, String key) =>
+      preferences.getString(key).split('|');
 
-  void _writeHistory(SharedPreferences preferences, String key, List<String> history) =>
+  void _writeHistory(
+          SharedPreferences preferences, String key, List<String> history) =>
       preferences.setString(key, history.join('|'));
 
   String _getPrevious(Map<String, List<String>> versionTrail, String key) {
